cc122b06c07c7699b5fb25bbe223c0a6e81fc2de
PrasathB-27 <90265595+PrasathB-27@users.noreply.github.com>
2022-01-17 12:23:10 +0530

    Add files via upload

---
+++ chatcopy.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width-device-width, initial-scale=1.0">
+      <title>Chatbot</title>
+      <link rel="stylesheet" href="chatindex.css">
+      <script src="https:kit.fontawesome.com/a076d05399.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="title">simple Chatbot</div>
+            <div class="form">
+               <div class="bot-index inbox">
+                    <div class="icon">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="msg-header">
+                        <p>Hello,how can i help u                          
+                        </p>
+                    </div>
+               </div>
+            </div>
+            <div class="type-field">
+                <div class="input-data">
+                    <input id="data" type="text" placeholder="Type something here.." required>
+                    <button id="send-btn">send</button>
+                </div>
+            </div>
+        </div>
+        <script>
+            $(document).ready(function(){
+              $("#send-btn").on("click",function(){
+                  $value = $("#data").val();
+                  $msg = '<div class="user-index inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                  $(".form").append($msg);
+                  $("#data").val('');
+
+                //   ajax code
+                  $.ajax({
+                      url: 'message.php',
+                      type: 'POST',
+                      data: 'text='+$value,
+                      success: function(result){
+                          $replay=' <div class="bot-index inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                          $(".form").append($replay); 
+                        //   when chat goes down the scroll bar automatically comes to the bottom
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                  });
+                });
+            });
+        </script>
+    </body>
+</html>
